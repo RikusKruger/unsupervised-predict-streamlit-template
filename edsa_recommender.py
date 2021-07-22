e000b68ac5057f7cc4bd2b761b50d75a743934e8
Rikus Kruger <rikusetiennekruger@gmail.com>
2021-07-22 21:35:03 +0200

    final commit

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -87,18 +87,10 @@ imdb_df['title_cast'] = imdb_df['title_cast'].str.split('|')
 # title_cast = title_cast.reset_index([0, 'movieId'])
 # title_cast.columns = ['movieId', 'title_cast']
 
-        
 
+filter_list = ['Content Based Filtering', 'Collaborative Based Filtering']
 
-
-
-
-filter_list = ['Content Based Filtering', 'Collaborative Based Filtering', '1']
-
-
-
-
-
+ 
 # App declaration
 def main():
 
@@ -111,19 +103,65 @@ def main():
     # -------------------------------------------------------------------
     st.set_page_config(layout="wide")
     page_selection = st.sidebar.selectbox("Choose Option", page_options)
+
     if page_selection == "Recommender System":
         # Header contents
         st.write('# Movie Recommender Engine')
         st.write('### EXPLORE Data Science Academy Unsupervised Predict')
-        # st.image('resources/imgs/popcorn.png',use_column_width=True)
-        st.image('resources/videos/popcorn_gif_2.gif',use_column_width=True)
-        # st.video('https://www.youtube.com/watch?v=To4f-ODZmIE')
-
+        st.image('resources/imgs/Image_header.png',use_column_width=True)
         # Recommender System algorithm selection
-
-        sys = st.radio("Select an algorithm", filter_list)
+        sys = st.radio("Select an algorithm",
+                       ('Content Based Filtering',
+                        'Collaborative Based Filtering'))
 
         # User-based preferences
+        st.write('### Enter Your Three Favorite Movies')
+        movie_1 = st.selectbox('Fisrt Option',title_list[14930:15200])
+        movie_2 = st.selectbox('Second Option',title_list[25055:25255])
+        movie_3 = st.selectbox('Third Option',title_list[21100:21200])
+        fav_movies = [movie_1,movie_2,movie_3]
+
+        # Perform top-10 movie recommendation generation
+        if sys == 'Content Based Filtering':
+            if st.button("Recommend"):
+                try:
+                    with st.spinner('Crunching the numbers...'):
+                        top_recommendations = content_model(movie_list=fav_movies,
+                                                            top_n=10)
+                    st.title("We think you'll like:")
+                    for i,j in enumerate(top_recommendations):
+                        st.subheader(str(i+1)+'. '+j)
+                except:
+                    st.error("Oops! Looks like this algorithm does't work.\
+                              We'll need to fix it!")
+
+
+        if sys == 'Collaborative Based Filtering':
+            if st.button("Recommend"):
+                try:
+                    with st.spinner('Crunching the numbers...'):
+                        top_recommendations = collab_model(movie_list=fav_movies,
+                                                           top_n=10)
+                    st.title("We think you'll like:")
+                    for i,j in enumerate(top_recommendations):
+                        st.subheader(str(i+1)+'. '+j)
+                except:
+                    st.error("Oops! Looks like this algorithm does't work.\
+                              We'll need to fix it!")
+
+
+    # -------------------------------------------------------------------
+
+    # ------------- SAFE FOR ALTERING/EXTENSION -------------------
+    if page_selection == "Solution Overview":
+        st.title("Solution Overview")
+        st.write("Describe your winning approach on this page")
+
+
+
+    if page_selection == "Popcorn":
+        st.image('resources/videos/popcorn_gif_2.gif',use_column_width=True)
+
         st.write('### Enter Your Three Favorite Movies')
         col1, col2, col3 = st.beta_columns(3)
 
@@ -148,45 +186,18 @@ def main():
 
             movies_titles[col] = col_dict[key].selectbox(f'Select movie {col + 1}:', genre_year_movies_temp['title'].to_list())
             movies_ids[col] = movies_df[movies_df['title'] == movies_titles[col]]["movieId"].values
-            
 
 
-        # Perform top-10 movie recommendation generation
-        if sys == 'Content Based Filtering':
-            if col1.button("Recommend"):
-                try:
-                    with st.spinner('Crunching the numbers...'):
-                        top_recommendations = content_model(movies_titles,
-                                                            top_n=10)
-                    st.title("We think you'll like:")
-                    for i,j in enumerate(top_recommendations):
-                        st.subheader(str(i+1)+'. '+j)
-                except:
-                    st.error("Oops! Looks like this algorithm does't work.\
-                              We'll need to fix it!")
 
+        if col1.button("Recommend"):
+            # try:
+                with st.spinner('Crunching the numbers...'):
+                    top_recommendations = collab_model(movies_titles,
+                                                        top_n=10)
+                # st.write(top_recommendations)
 
-        if sys == 'Collaborative Based Filtering':
-            if col1.button("Recommend"):
-                try:
-                    with st.spinner('Crunching the numbers...'):
-                        top_recommendations = collab_model(movie_list=movies_ids,
-                                                           top_n=10)
-                    st.title("We think you'll like:")
-                    for i,j in enumerate(top_recommendations):
-                        st.subheader(str(i+1)+'. '+j)
-
-                except:
-                    st.error("Oops! Looks like this algorithm does't work.\
-                              We'll need to fix it!")
-        
-        
-        if sys == '1':
-            recommend = col1.button("Recommend")
-            if recommend:
-                predicted_movies_ids = content_model(movies_titles, 10)
-                st.write(predicted_movies_ids)
-                # predicted_movies_ids = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+                pred_ids = movies_df[movies_df["title"].isin(top_recommendations)]
+                # st.write(f"preds = {pred_ids['movieId'].values}")
                 new_col1, new_col2, new_col3, new_col4, new_col5 = st.beta_columns(5)
 
                 new_col_dict = {f'col_0': new_col1,
@@ -195,39 +206,30 @@ def main():
                                 f'col_3': new_col4,
                                 f'col_4': new_col5}
 
-                for i, id in enumerate(predicted_movies_ids[:5]):
+                for i, id in enumerate(pred_ids[:5].values):
                     
+                    # new_col_dict[f'col_{i}'].write(id)
+                    title_dict = {f'movie_{id[0]}_title': id[1]}
+                    # st.write(id[0])
 
-                    new_col_dict[f'col_{i}'].write(id)
-                    title_dict = {f'movie_{id}_title': movies_df[movies_df["movieId"] == id]["title"].values[0]}
-                    director_dict = {f'movie_{id}_director': imdb_df[imdb_df["movieId"] == id]["director"].values[0]}
-                    cast_dict = {f'movie_{id}_cast': imdb_df[imdb_df["movieId"] == id]["title_cast"].values[0]}
+                    # try:
+                    director_dict = {f'movie_{id[0]}_director': imdb_df[imdb_df["movieId"] == id[0]]["director"]}
+                    # st.markdown(f'director dict = {director_dict}')
+                    cast_dict = {f'movie_{id[0]}_cast': imdb_df[imdb_df["movieId"] == id[0]]["title_cast"]}
 
-                    search_keyword = title_dict[f'movie_{id}_title'].replace(' ', '+')
+                    search_keyword = title_dict[f'movie_{id[0]}_title'].replace(' ', '+')
                     html = urllib.request.urlopen("https://www.youtube.com/results?search_query=" + search_keyword + "+trailer")
                     video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
 
-                    new_col_dict[f'col_{i}'].title(title_dict[f'movie_{id}_title'])
+                    new_col_dict[f'col_{i}'].title(title_dict[f'movie_{id[0]}_title'])
                     new_col_dict[f'col_{i}'].video("https://www.youtube.com/watch?v=" + video_ids[0])
                     new_col_dict[f'col_{i}'].subheader("Director:")
-                    new_col_dict[f'col_{i}'].write(f" - {director_dict[f'movie_{id}_director']}")
+                    new_col_dict[f'col_{i}'].write(f" - {director_dict[f'movie_{id[0]}_director'].values[0]}")
 
                     new_col_dict[f'col_{i}'].subheader("Cast:")
-                    for member in cast_dict[f'movie_{id}_cast']:
-                        new_col_dict[f'col_{i}'].write(f" - {member}\n")   
-
-
-
-# -------------------------------------------------------------------
-
-# ------------- SAFE FOR ALTERING/EXTENSION -------------------
-    if page_selection == "Solution Overview":
-        st.title("Solution Overview")
-        st.write("Describe your winning approach on this page")
-
-
-                    # You may want to add more sections here for aspects such as an EDA,
-                    # or to provide your business pitch.
+                    for member in cast_dict[f'movie_{id[0]}_cast']:
+                        for name in member:
+                            new_col_dict[f'col_{i}'].write(f" - {name}\n")   
 
 
 if __name__ == '__main__':
--- recommenders/content_based.py
+++ recommenders/content_based.py
@@ -1,156 +1,287 @@
 """
-
     Content-based filtering for item recommendation.
-
     Author: Explore Data Science Academy.
-
     Note:
     ---------------------------------------------------------------------
     Please follow the instructions provided within the README.md file
     located within the root of this repository for guidance on how to use
     this script correctly.
-
     NB: You are required to extend this baseline algorithm to enable more
     efficient and accurate computation of recommendations.
-
     !! You must not change the name and signature (arguments) of the
     prediction function, `content_model` !!
-
     You must however change its contents (i.e. add your own content-based
     filtering algorithm), as well as altering/adding any other functions
     as part of your improvement.
-
     ---------------------------------------------------------------------
-
     Description: Provided within this file is a baseline content-based
     filtering algorithm for rating predictions on Movie data.
-
 """
 
 # Script dependencies
-from operator import index
+from operator import truediv
 import os
+from numpy.lib.npyio import load
 import pandas as pd
 import numpy as np
+import pickle
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import CountVectorizer
 
+from nltk.stem.snowball import SnowballStemmer
 
-# Importing data
-# df_sample_submission = pd.read_csv('sample_submission.csv')
-df_movies = pd.read_csv('resources/data/movies.csv')
+# # Importing data
+df_movies = pd.read_csv('resources/data/movies.csv', sep = ',',delimiter=',')
+ratings = pd.read_csv('resources/data/ratings.csv')
 df_imdb = pd.read_csv('resources/data/imdb_data.csv')
-df_genome_scores = pd.read_csv('resources/data/genome_scores.csv')
-df_genome_tags = pd.read_csv('resources/data/genome_tags.csv')
-df_train = pd.read_csv('resources/data/train.csv')
-df_test = pd.read_csv('resources/data/test.csv')
-df_tags = pd.read_csv('resources/data/tags.csv')
-df_links = pd.read_csv('resources/data/links.csv')
+# df_movies.dropna(inplace=True)
 
 
-# make a copy of the train dataset to work on
-train_copy = df_train.copy()
-# remove the timestamp column from the copy in order to be able to build models on train data that matches the test data
-# we will evaluate the importance of the time stamp column later
-train_copy = train_copy.drop('timestamp', axis = 1)
 
-# function to preprocess data
-# we use a subset of the data for computation purposes
-def data_preprocessing (subset_size = 12000):
+def storeData(db, filename):
+    dbfile = open(f'{filename}', 'ab')
+    # source, destination
+    pickle.dump(db, dbfile)                     
+    dbfile.close()
+    return print("Stored")
+  
+def loadData(filename):
+    # for reading also binary mode is important
+    dbfile = open(f'{filename}', 'rb')     
+    db = pickle.load(dbfile)
+    dbfile.close()
+    return db
+
+# # data_1 = data_preprocessing(0, 31211)
+# data_1 = loadData('data_1')
+# print("data_1 loaded\n\n")
+
+# # data_2 = data_preprocessing(31211, 62423)
+
+# data_2 = loadData('data_2')
+# print("data_2 loaded\n\n")
+
+
+# count_vec = CountVectorizer()
+
+# count_matrix_1 = count_vec.fit_transform(data_1['combined_features'])
+# cosine_sim_1 = cosine_similarity(count_matrix_1, count_matrix_1)
+# cosine_sim_1 = loadData('cosine_sim_1')
+# print("cosine_sim_1 loaded\n\n")
+
+
+# # count_matrix_2 = count_vec.fit_transform(data_2['combined_features'])
+# # cosine_sim_2 = cosine_similarity(count_matrix_2, count_matrix_2)
+# cosine_sim_2 = loadData('cosine_sim_2')
+# print("cosine_sim_2 loaded\n\n")
+
+
+def data_preprocessing(subset_size=28000):
     """Prepare data for use within Content filtering algorithm.
-
     Parameters
     ----------
     subset_size : int
         Number of movies to use within the algorithm.
-
     Returns
     -------
     Pandas Dataframe
         Subset of movies selected for content-based filtering.
-
     """
-
-    
-    # Inner join the imdb dataframe with the movies dataframe
     imdb = df_imdb[['movieId','title_cast','director', 'plot_keywords']]
-    merge = imdb.merge(df_movies[['movieId', 'genres', 'title']], on='movieId', how='inner')
+    merge = df_movies.merge(imdb[['movieId','title_cast','director', 'plot_keywords']], on='movieId', how='left')
 
     # Convert data types to string in order to do string manipulation
     merge['title_cast'] = merge.title_cast.astype(str)
     merge['plot_keywords'] = merge.plot_keywords.astype(str)
-    merge['genres'] = merge.genres.astype(str)
+    # merge['genres'] = merge.genres.astype(str)
     merge['director'] = merge.director.astype(str)
 
+    df_cbr = pd.DataFrame()
+
+    # handle cast
     # clean directors and title_cast column
     # remove spaces and "|"
-    merge['director'] = merge['director'].apply(lambda x: "".join(x.lower() for x in x.split()))
-    merge['title_cast'] = merge['title_cast'].apply(lambda x: "".join(x.lower() for x in x.split()))
-    merge['title_cast'] = merge['title_cast'].map(lambda x: x.split('|'))
     #convert title cast back to string and remove commas
-    merge['title_cast'] = merge['title_cast'].apply(lambda x: ','.join(map(str, x)))
-    merge['title_cast'] = merge['title_cast'].replace(',',' ', regex=True)
-    
-    # clean plot keywords column
-    # remove spaces and "|"
-    merge['plot_keywords'] = merge['plot_keywords'].map(lambda x: x.split('|'))
-    merge['plot_keywords'] = merge['plot_keywords'].apply(lambda x: " ".join(x))
+    df_cbr['cast'] = merge['title_cast'].apply(lambda x: x.split('|'))
 
-    # clean plot genres column
-    # remove spaces and "|" 
-    merge['genres'] = merge['genres'].map(lambda x: x.lower().split('|'))
-    merge['genres'] = merge['genres'].apply(lambda x: " ".join(x))
+    df_cbr['cast'] = df_cbr['cast'].apply(lambda x: ','.join(map(str, x)))
+    df_cbr['cast'] = df_cbr['cast'].replace(',',' ', regex=True)
 
-    
-    #subset table to only return required columns
-    df_features = merge[['title_cast','director','plot_keywords','genres']]
+    df_cbr['director'] = merge['director'].apply(lambda x: "".join(x.lower() for x in x.split()))
 
-    #we combine the features columns into  single string
-    merge['combined_features'] = df_features['title_cast'] +' '+ df_features['director'] +' '+ df_features['plot_keywords'] +' '+ df_features['genres']
-    merge_subset = merge[:subset_size]
-    
-    return merge_subset
+    # handle keyword
+    stemmer = SnowballStemmer('english')
+    df_cbr['keyword'] = merge['plot_keywords'].apply(lambda x: x.split('|'))
+
+    df_cbr['keyword'] = df_cbr['keyword'].map(lambda x: [i.split(' ') for i in x])
+    df_cbr['keyword'] = df_cbr['keyword'].map(lambda x: [item for sublist in x for item in sublist])
 
 
-# Preprocess data
-processed_df = data_preprocessing(12000)
-print(processed_df.columns)
-#define the count vectorizer
-cv = CountVectorizer()
+    # df_cbr['keyword'] = df_cbr['keyword'].apply(lambda x: [eval(i) for i in x])
+    df_cbr['keyword'] = df_cbr['keyword'].apply(lambda x: [stemmer.stem(i) for i in x])
+    df_cbr['keyword'] = df_cbr['keyword'].apply(lambda x: [str.lower(i.replace(" ", "")) for i in x])
+    # handle genres
 
+
+    df_cbr['genre'] = merge['genres'].map(lambda x: x.lower().split('|'))
+    df_cbr['genre'] = df_cbr['genre'].apply(lambda x: " ".join(x))
+
+    # handle title
+    df_cbr['title'] = merge['title']
+
+    # merge all
+    df_cbr['mixed'] = df_cbr['keyword'].astype(str) + df_cbr['cast'].astype(str) + df_cbr['genre'].astype(str) + df_cbr['director'].astype(str)
+
+    df_cbr_1 = df_cbr.iloc[:subset_size]
+    df_cbr_2 = df_cbr.iloc[subset_size:]
+
+
+    return df_cbr_1, df_cbr_2
+
+
+df_cbr_1, df_cbr_2 = data_preprocessing()
 # !! DO NOT CHANGE THIS FUNCTION SIGNATURE !!
 # You are, however, encouraged to change its content.  
-#function to obtain recommendations
-def content_model(titles,n):
-    '''
-    title: title that user will enter
-    n: the number of recommendations required
-    cv_matrix: unpickled countvecorizer
-    
-    '''
-    df = processed_df.copy()
-    cv_model = cv.fit_transform(df['combined_features'])
-    #set title column as the index and create a dataframe of titles
-    df.set_index('title', inplace = True)
-    indices = pd.DataFrame(df.index)
-    #create the cosine similarity matrix using the count vectorizer
-    sim_score = cosine_similarity(cv_model,cv_model)
-    
-    #create an empty list of the recommended movies
-    recommended_movies = []
-    
-    for title in titles:
-        # match the entered title to its index in the titles dataframe
-        idx = indices[indices == title].index[0]
+def content_model(movie_list,top_n=10):
+    """Performs Content filtering based upon a list of movies supplied
+       by the app user.
+    Parameters
+    ----------
+    movie_list : list (str)
+        Favorite movies chosen by the app user.
+    top_n : type
+        Number of top recommendations to return to the user.
+    Returns
+    -------
+    list (str)
+        Titles of the top-n movie recommendations to the user.
+    """
 
-        # get the similarity scores from highest to lowest  
-        score_series = pd.Series(sim_score[idx]).sort_values(ascending = False)
+    store = False
+    recommended = []
+    count = CountVectorizer(analyzer='word', ngram_range=(1, 2), min_df=0, stop_words='english')
+    count_matrix_1 = count.fit_transform(df_cbr_1['mixed'])
+    count_matrix_2 = count.fit_transform(df_cbr_2['mixed'])
+    if store == False:
+        return movie_list
 
-        # create a list of the top nth title indexes from the similarity matrix
-        top_n_indexes = list(score_series.iloc[1:int(n/3)].index)
-    
-        # add the titles that match the indexes to the list
-        for i in top_n_indexes:
-            recommended_movies.append(list(df.index)[i])
+    print("fit_transform done")
+    # count_matrix.todense()
+
+    if store:
+        # cosine_sim_1 = cosine_similarity(count_matrix_1, count_matrix_1)
+        # storeData(cosine_sim_1, 'cosine_sim_1')
+        # print("cosine_sim_1 stored\n\n")
+       
+        cosine_sim_1 = cosine_similarity(count_matrix_1)
+        storeData(cosine_sim_1, 'cosine_sim_1')
+        print("cosine_sim_1 stored\n\n")
+
+
+        cosine_sim_2 = cosine_similarity(count_matrix_2)
+        storeData(cosine_sim_2, 'cosine_sim_2')
+        print("cosine_sim_2 stored\n\n")
+
+        indices_1 = pd.Series(df_cbr_1.index, index=df_cbr_1['title'])
+        storeData(indices_1, 'indices_1')
+        print("indices_1 stored\n\n")
+
+        indices_2 = pd.Series(df_cbr_2.index, index=df_cbr_2['title'])
+        storeData(indices_2, 'indices_2')
+        print("indices_2 stored\n\n")
+
+        titles_1 = df_cbr_1['title']
+        storeData(titles_1, 'titles_1')
+        print("titles_1 stored\n\n")
+
+        titles_2 = df_cbr_2['title']
+        storeData(titles_2, 'titles_2')
+        print("titles_2 stored\n\n")
+
+    else:
         
-    return recommended_movies
+        cosine_sim_1 = loadData('cosine_sim_1')
+
+        cosine_sim_2 = loadData('cosine_sim_2')
+
+        indices_1 = loadData('indices_1')
+
+        indices_2 = loadData('indices_2')
+
+        titles_1 = loadData('titles_1')
+
+        titles_2 = loadData('titles_2')
+
+
+
+    for title in movie_list:
+        try:
+            idx = indices_1[title]
+            similarity_scores = list(enumerate(cosine_sim_1[idx]))
+            similarity_scores = sorted(similarity_scores, key=lambda x: x[1], reverse=True)
+            similarity_scores = similarity_scores[1:21]
+            movie_indices = [i[0] for i in similarity_scores]
+            recommended.append(titles_1.iloc[movie_indices[:4]])
+
+        except:
+            idx = indices_2[title]
+            similarity_scores = list(enumerate(cosine_sim_2[idx]))
+            similarity_scores = sorted(similarity_scores, key=lambda x: x[1], reverse=True)
+            similarity_scores = similarity_scores[1:21]
+            movie_indices = [i[0] for i in similarity_scores]
+            recommended.append(titles_2.iloc[movie_indices[:4]])
+    print(recommended[:top_n])
+
+    return recommended
+
+
+
+
+    # # Initializing the empty list of recommended movies
+    # recommended_movies = []
+
+
+    # # Getting the index of the movie that matches the title
+    # # print(f"I am printing here !!!!!!!!!!!!!!!!: \n\n{str(data[data['title'] == 'Toy Story (1995)']['movieId'][0])}")
+    # # print(f"I am printing here !!!!!!!!!!!!!!!!: \n\n{movie_list[0]}")
+
+    # try:
+    #     idx_1 = data_1[data_1['title'] == movie_list[0]]['movieId'].index[0]
+    #     rank_1 = cosine_sim_1[idx_1]
+    # except:
+    #     idx_1 = data_2[data_2['title'] == movie_list[0]]['movieId'].index[0]
+    #     rank_1 = cosine_sim_2[idx_1]
+
+    # try:
+    #     idx_2 = data_1[data_1['title'] == movie_list[1]]['movieId'].index[0]
+    #     rank_2 = cosine_sim_1[idx_2]
+    # except:
+    #     idx_2 = data_2[data_2['title'] == movie_list[1]]['movieId'].index[0]
+    #     rank_2 = cosine_sim_2[idx_2]
+
+    # try:
+    #     idx_3 = data_1[data_1['title'] == movie_list[2]]['movieId'].index[0]
+    #     rank_3 = cosine_sim_1[idx_3]
+    # except:
+    #     idx_3 = data_2[data_2['title'] == movie_list[2]]['movieId'].index[0]
+    #     rank_3 = cosine_sim_2[idx_3]
+
+
+    # # Creating a Series with the similarity scores in descending order
+    # # Calculating the scores
+    # score_series_1 = pd.Series(rank_1).sort_values(ascending = False)
+    # score_series_2 = pd.Series(rank_2).sort_values(ascending = False)
+    # score_series_3 = pd.Series(rank_3).sort_values(ascending = False)
+     
+    # # Getting the indexes of the 10 most similar movies
+    # listings = score_series_1.append(score_series_2).append(score_series_3).sort_values(ascending = False)
+
+    # # Store movie names
+    # recommended_movies = []
+    # # Appending the names of movies
+    # top_50_indexes = list(listings.iloc[1:50].index)
+    # # Removing chosen movies
+    # top_indexes = np.setdiff1d(top_50_indexes,[idx_1,idx_2,idx_3])
+    # for i in top_indexes[:top_n]:
+    #     recommended_movies.append(list(df_movies['title'])[i])
+    # return recommended_movies
